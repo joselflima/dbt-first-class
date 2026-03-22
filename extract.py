6ec0d8c2a2a412c04034aa6a5c468c4894aafc04
joselflima <lucasferlima8@gmail.com>
2026-03-21 23:44:04 -0300

    chore: configured extract Spotify Kaggle data

--- extract.py
+++ extract.py
@@ -2,20 +2,18 @@ import kagglehub # type: ignore
 import shutil
 import os
 
-path = kagglehub.dataset_download("abbas829/bank-customer-churn")
+path = kagglehub.dataset_download("emanfatima2/spotify-global-hits-and-artist-analytics")
 destination = "./seeds"
+
+print(path)
 
 try:
     # If path contains a single directory (like '1/'), extract its contents
     contents = os.listdir(path)
-    if len(contents) == 1 and os.path.isdir(os.path.join(path, contents[0])):
-        inner_path = os.path.join(path, contents[0])
-        for item in os.listdir(inner_path):
-            shutil.move(os.path.join(inner_path, item), os.path.join(destination, item))
-    else:
-        # Move all contents directly
-        for item in os.listdir(path):
-            shutil.move(os.path.join(path, item), os.path.join(destination, item))
+
+    for item in os.listdir(path):
+        shutil.move(os.path.join(path, item), os.path.join(destination, item))
+        
     print(f"Moved files to {destination}")
 except Exception as e:
     print(f"An error occurred: {e}")
